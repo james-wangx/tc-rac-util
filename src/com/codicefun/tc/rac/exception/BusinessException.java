1df5dce33f137663eded3d83d4c5d9e441e7c6b6
James Wang <james_wangx@163.com>
2025-09-05 10:15:05 +0800

    Add QueryUtil

---
+++ src/com/codicefun/tc/rac/exception/BusinessException.java
@@ -0,0 +1,9 @@
+package com.codicefun.tc.rac.exception;
+
+public class BusinessException extends RuntimeException {
+
+	public BusinessException(String message) {
+		super(message);
+	}
+
+}
--- src/com/codicefun/tc/rac/handler/TestHandler.java
+++ src/com/codicefun/tc/rac/handler/TestHandler.java
@@ -3,14 +3,17 @@ package com.codicefun.tc.rac.handler;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 
-import com.teamcenter.rac.util.MessageBox;
+import com.teamcenter.rac.aif.AbstractAIFUIApplication;
+import com.teamcenter.rac.aifrcp.AIFUtility;
+import com.teamcenter.rac.kernel.TCSession;
 
 public class TestHandler extends AbstractHandler {
 
+	private static final AbstractAIFUIApplication application = AIFUtility.getCurrentApplication();
+	private static final TCSession session = (TCSession) application.getSession();
+
 	@Override
 	public Object execute(ExecutionEvent event) {
-		System.out.println("Start TestHandler");
-		MessageBox.post("这是个测试消息", "提示", MessageBox.INFORMATION);
 		return null;
 	}
 
